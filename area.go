9b340d7acceba67d4d85b2d9481d3d37fc5db064
Ilya Yalovets <yaloveczilya@gmail.com>
2023-10-09 15:21:57 +0300

    Добавлена

--- area.go
+++ area.go
@@ -4,6 +4,10 @@ import (
 	"math"
 )
 
+func GetRectangleArea(width, height float32) float32 {
+	return width * height
+}
+
 func GetCircleArea(radius float32) float32 {
 	return math.Pi * radius * radius
 }
