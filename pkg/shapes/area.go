bc6b1536fc8743ff1b28ae7b71821d2fb2db8666
Ilya Yalovets <yaloveczilya@gmail.com>
2023-10-09 12:10:50 +0300

    Добавлена V3.0.0

--- pkg/shapes/area.go
+++ pkg/shapes/area.go
@@ -1,10 +1,6 @@
-package calculate
+package shapes
 
 import "math"
-
-func GetRectangleArea(width, height float32) float32 {
-	return width * height
-}
 
 func GetCircleArea(radius float32) float32 {
 	return math.Pi * radius * radius
